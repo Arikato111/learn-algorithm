9714a78101079c03ced77a66b9d3fcfdf2485e39
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-03-14 15:38:31 +0700

    fix gen::<u8> to make it faster

--- quick_sort/rust/src/main.rs
+++ quick_sort/rust/src/main.rs
@@ -1,26 +1,30 @@
-#[allow(unused)]
-use thread_sort::{normal_sort::normal_quick_sort, sort::quick_sort};
 use rand::Rng;
 use std::{
     sync::{Arc, Mutex},
     time::Instant,
 };
+#[allow(unused)]
+use thread_sort::{normal_sort::normal_quick_sort, sort::quick_sort};
 
-const ARR_LEN: usize = 10_000_000;
+const ARR_LEN: usize = 100_000_000;
 fn main() {
     let mut random = rand::thread_rng();
     let arr = Arc::new(Mutex::new(vec![0u32; ARR_LEN]));
     for i in 0..ARR_LEN {
-        arr.lock().unwrap()[i] = random.gen::<u8>() as u32;
+        /*
+         *  arr.lock().unwrap()[i] = random.gen::<u8>() as u32;
+         *  gen with u8 and change to u32 make it slower.
+         */
+        arr.lock().unwrap()[i] = random.gen::<u32>();
     }
-    // quick_sort_node(&mut arr, 0, arr_len);
-    // println!("{:?}", arr.lock().unwrap());
-    let s1 = Instant::now();
-    // 0 is 62
-    // quick_sort(&arr, 4); // 1839.10849619s
-    quick_sort(&arr, 0);  // 1892.023405653s
-    // let a = arr.clone();
-    // normal_quick_sort(&mut a.lock().unwrap());
+    let s1 = Instant::now(); // time counter
+    // quick_sort(&arr, 4);
+    quick_sort(&arr, 4);
+    /*
+     * array 10^8 elements.
+     * quick sort 1 thread: 10s
+     * quick sort 16 thread: 11s
+     */
     println!("{:?}", s1.elapsed());
     // println!("{:?}", arr.lock().unwrap());
 }
